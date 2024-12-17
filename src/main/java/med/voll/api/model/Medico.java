9dedffeda484438b5a629febc29c10fd1c82ce8d
Daniel Romero <dandres508@gmail.com>
2024-12-17 14:45:19 -0500

    modelos

---
+++ src/main/java/med/voll/api/model/Medico.java
@@ -0,0 +1,29 @@
+package med.voll.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.dto.common.Direccion;
+import med.voll.api.dto.medico.Especialidad;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String nombre;
+    private String email;
+    private String documento;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion direccion;
+}
